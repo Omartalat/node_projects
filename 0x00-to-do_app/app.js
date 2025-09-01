a95e14ba3b60f79e18be960b77ac052c0c911b3b
omar-talat <dr.omartalat@gmail.com>
2025-09-01 16:49:02 +0300

    Refactor To-Do app routes for improved clarity and fix delete functionality

--- 0x00-to-do_app/app.js
+++ 0x00-to-do_app/app.js
@@ -11,24 +11,33 @@ app.get("/todos", (req, res) => {
 });
 
 app.post("/todos", (req, res) => {
-  const title = req.body;
+  const { title } = req.body;
   const todo = { id: idCounter++, title, completed: false };
   todos.push(todo);
-  res.status(201).json(todos);
+  res.status(201).json(todo);
 });
 
 app.put("/todos/:id", (req, res) => {
-  const id = req.params;
+  const { id } = req.params;
   const { title, completed } = req.body;
-  const todo = todos.find((t) => t.id === id);
-  if (!todo) res.status(404).json({ error: "To-Do task is not found" });
+  const todo = todos.find((t) => t.id === parseInt(id));
+  if (!todo) return res.status(404).json({ error: "To-Do task is not found" });
 
   todo.title = title !== undefined ? title : todo.title;
   todo.completed = completed !== undefined ? completed : todo.completed;
   res.json(todo);
 });
 
-PORT = 8000;
+app.delete("/todos/:id", (req, res) => {
+  const { id } = req.params;
+  const todo = todos.find((t) => t.id === parseInt(id));
+  if (!todo) return res.status(404).json({ error: "To-Do task is not found" });
+  const index = todos.indexOf(todo);
+  todos.splice(index, 1);
+  res.json(todo);
+});
+
+const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
