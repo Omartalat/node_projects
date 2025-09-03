35bec802494add1615f7ad7dd7522b127a9499c8
omar-talat <dr.omartalat@gmail.com>
2025-09-03 15:14:10 +0300

    fix: update shortenUrl function to handle async operations and use environment variable for PORT

--- 0x02-url_shortner/controllers/urlController.js
+++ 0x02-url_shortner/controllers/urlController.js
@@ -2,22 +2,29 @@ const { nanoid } = require("nanoid");
 
 const db = require("../database");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-exports.shortenUrl = (req, res) => {
+exports.shortenUrl = async (req, res) => {
   const { original_url } = req.body;
-  const short_url = nanoid(6);
+  let short_url;
 
-  db.run(
-    "INSERT INTO urls(short_url, original_url) VALUES(?, ?)",
-    [short_url, original_url],
-    (err) => {
-      if (err) {
-        return res
-          .status(500)
-          .json({ error: "Database error", details: err.message });
-      }
-      res.json({ shortUrl: `http://localhost:${PORT}/${short_url}` });
-    }
+  row = await db.get(
+    "SELECT * FROM urls WHERE original_url = ?",
+    [original_url]
   );
+
+  if (row && row.short_url)
+    return res.json({ shortUrl: `http://localhost:${PORT}/${row.short_url}` });
+
+  let exists = true;
+  while (exists) {
+    short_url = nanoid(6);
+    let row = await db.get("SELECT * FROM urls WHERE short_url = ?", [short_url]);
+    exists = !!row;
+  }
+  await db.run("INSERT INTO urls(short_url, original_url) VALUES(?, ?)", [
+    short_url,
+    original_url,
+  ]);
+  res.json({ shortUrl: `http://localhost:${PORT}/${short_url}` });
 };
